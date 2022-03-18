613d1780aca59652ab73b155832437976d86d190
Alex Bates <alexlbates@gmail.com>
2022-03-18 19:41:02 -0400

    add ttl for r53 record

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -2,4 +2,6 @@ resource "aws_route53_record" "primary-website-record" {
   name    = "" # bare record will control base zone url
   type    = "A"
   zone_id = data.aws_route53_zone.primary-domain.id
+
+  ttl = 60
 }
