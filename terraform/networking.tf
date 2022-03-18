65c06f7e01e02c25f1e37e98dc882388c925e958
Alex Bates <alexlbates@gmail.com>
2022-03-18 19:46:38 -0400

    okay actually this time.

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -4,7 +4,7 @@ resource "aws_route53_record" "primary-website-record" {
   zone_id = data.aws_route53_zone.primary-domain.id
 
   alias {
-    name                   = aws_s3_bucket.website-bucket.bucket
+    name                   = aws_s3_bucket.website-bucket.website_endpoint
     zone_id                = aws_s3_bucket.website-bucket.hosted_zone_id
     evaluate_target_health = true
   }
