685bb4ca35a92885948a13153ee6486525083892
Alex Bates <alexlbates@gmail.com>
2022-03-26 17:12:30 -0400

    update project page with a few items, mostly to make sure it works at all.

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,12 +1,30 @@
+import './Projects.css'
+
 const projectList = [
-    {name: 'Test', description: 'this is a test', link: ''},
+    {
+        name: 'Kaleidoscope',
+        description: 'Random media player',
+        link: '//kaleidoscope.alexlbates.com'
+    },
+    {
+        name: 'Developer Setup',
+        description:
+            'A one stop shop for common workspace configurations and scripts that I would want to carry between machines. ' +
+            'This includes anything like support scripts or shell configurations that are kind of annoying to set up again and again.',
+        link: '//gitlab.com/alloba/developer-environment-configs'
+    },
+    {
+        name: 'WSG Scraper',
+        description: 'Download Webm files from 4chan based on thread and search-term, and automatically upload to S3.',
+        link: 'TODO_SECURE_THIS_PROJECT'
+    },
 ]
 
 
 export default function Projects() {
     return (
-        <div>
-            <table>
+        <div >
+            <table className={'project-table-div'}>
                 <tr>
                     <th>Name</th>
                     <th>Description</th>
@@ -39,14 +57,14 @@ function renderSingleProjectListElement(projectItem) {
     if (projectItem.link) {
         return (
             <tr key={projectItem.name}>
-                <td><a href={projectItem.link}>{projectItem.name}</a></td>
+                <td><a className={'list-item'} href={projectItem.link}>{projectItem.name}</a></td>
                 <td>{projectItem.description}</td>
             </tr>
         )
     } else {
         return (
             <tr key={projectItem.name}>
-                <td>{projectItem.name}</td>
+                <td className={'list-item-noclick'}>{projectItem.name}</td>
                 <td>{projectItem.description}</td>
             </tr>
         )
