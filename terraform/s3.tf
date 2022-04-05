8f5aa780a29e30f0b9d453bb17a368865403fef3
Alex Bates <alexlbates@gmail.com>
2022-04-04 22:30:59 -0400

    Add a todo to deal with direct subpath page requests.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -12,8 +12,15 @@ resource "aws_s3_bucket_website_configuration" "website-access-config" {
   index_document {
     suffix = "index.html"
   }
+  
 }
 
+#TODO: All subroutes fail when coming into s3, since 
+#      it reports a 404. All requests need to go back to 
+#      index.html. CloudFront is a proper solution to this, 
+#      but if i still feel like i don't want to add a piece 
+#      of infrastructure to it, there is a solution s3 routing rules.
+#      https://via.studio/journal/hosting-a-reactjs-app-with-routing-on-aws-s3
 resource "aws_s3_bucket_website_configuration" "www-forwarding" {
     bucket = aws_s3_bucket.www.bucket
     redirect_all_requests_to  {
