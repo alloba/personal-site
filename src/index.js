674aa4706b3e17bbb341d59aacf1b29bc7289c71
Alex Bates <alexlbates@gmail.com>
2022-04-07 14:24:48 -0400

    eslint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {ChakraProvider, ColorModeScript} from "@chakra-ui/react";
 import theme from "./theme";
 
