2ac8d059a48f66befd3d5e2a347b242c0b344a79
Alex Bates <alexlbates@gmail.com>
2022-04-08 15:12:37 -0400

    add contents to kaleidoscope project page.

--- src/routes/Projects/ProjectKaleidoscope.js
+++ src/routes/Projects/ProjectKaleidoscope.js
@@ -1,7 +1,25 @@
-import {Box, Text} from "@chakra-ui/react";
+import {Box, Center, Heading, Image, Link, Text} from "@chakra-ui/react";
 
-const ProjectKaleidoscope = () =>(
-    <Box><Text>Kaleidoscope Here</Text></Box>
+const ProjectKaleidoscope = () => (
+    <Box>
+        <Heading textAlign={'center'}>Kaleidoscope</Heading>
+        <Text>A website meant to endlessly play little video clips in a random order.</Text>
+        <Text>
+            The images are pulled from a publicly accessible bucket in S3,
+            which are populated by a simple script that scrapes image boards online.
+        </Text>
+        <Text>
+            This project probably holds the record for the most used thing I have made in my personal time.
+            It's nice to just set it going on a monitor and have it as background noise.
+        </Text>
+
+        <Text><Link href={'//kaleidoscope.alexlbates.com'} color={'blue.200'}>Project Link</Link></Text>
+        <Text><Link href={'https://gitlab.com/alloba/kaleidoscope-combined'} color={'blue.200'}>Source Code</Link></Text>
+
+        <Center>
+            <Image src={'images/kaleidoscope/sampler.png'} maxWidth={'80%'}/>
+        </Center>
+    </Box>
 )
 
 export default ProjectKaleidoscope
